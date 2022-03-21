fa549450fbf3b40983aadeb7ebcb52702d54925f
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-03-21 15:02:27 +0500

    Create zss.py

---
+++ zss.py
@@ -0,0 +1,13 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from zsb import bnsbuy
+    bnsbuy()
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
