e9240f86e540aafee181d58ac20af2551eeeacc5
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2022-04-05 15:23:25 +0500

    Add files via upload

--- SSB.py
+++ SSB.py
@@ -7,7 +7,7 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from zsb import bnsbuy
-    bnsbuy()
+    from zsb import login
+    login()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
