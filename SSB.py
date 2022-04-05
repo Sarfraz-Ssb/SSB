6737617d0afa5dd3effbf28d5fac54d0fa5b0cee
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2022-04-05 15:27:02 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -7,7 +7,7 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from zsb import login
-    login()
+    from zsb import menu
+    menu()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
