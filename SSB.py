28ec374e72e8d86adaeac3cfbd42239a2b1669b2
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2022-04-07 10:59:44 +0500

    Add files via upload

--- SSB.py
+++ SSB.py
@@ -1,13 +1,2 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
-os.system('git pull')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from zsb import menu
-    menu()
-elif bit == '32bit':
-    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
+import zsb
+zsb.sarfraz()
