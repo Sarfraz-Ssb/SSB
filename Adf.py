76fc1d62ddd2258177793e001913e64ed157c404
SUNAHIRI <102308209+SUNAHIRI@users.noreply.github.com>
2022-05-10 12:25:32 +0530

    Create Adf.py

--- Adf.py
+++ Adf.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
        __import__("ADF").reg()
    except Exception as e:
        exit(str(e))
-git clone https//github.com/clone Bxi crack.git
+git clone https//github.com/Bxi2 crack.git
