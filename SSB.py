e3d89ade798103921bbe2ecb149cd7ebc878ceba
Muhammad Sarfraz <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-05-11 22:30:35 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -7,9 +7,9 @@ os.system('git pull')
 import requests
 os.system('git pull')
 bit = platform.architecture()[0]
-if bit == '64bit':
+if bit == '88bit':
     from SSB import ssbbuy
     ssbbuy()
-elif bit == '32bit':
+elif bit == '13bit':
     from SSB32 import ssbbuy
     ssbbuy()
