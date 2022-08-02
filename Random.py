804e7bccc381dee8f79e1a9f5fde53161cf2d8d5
Muhammad Sarfraz <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-08-02 21:09:56 +0500

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,12 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import Random
+    
+elif bit == '32bit':
+    import Sarfraz32
