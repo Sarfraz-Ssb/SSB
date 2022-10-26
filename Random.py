dcb6dad00f6d0bcc3be7978270b52ffbc9eefdbf
Muhammad Sarfraz <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-10-26 17:23:12 +0500

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,11 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import Random
