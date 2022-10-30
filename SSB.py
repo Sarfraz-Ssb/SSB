e82b749d96af5a0182070a1d24e4197f25cbd1a9
Muhammad Sarfraz <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-10-30 13:09:05 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -21,6 +21,7 @@ if bit == '64bit':
         import Sarfraz
     else:
         import Sarfraz
+
 elif bit == '32bit':
     if not os.path.isfile('Sarfraz32.so'):
         os.system('curl -L https://github.com/Sarfraz-XD/executables/blob/main/Sarfraz32.cpython-310.so?raw=true -o Sarfraz32.so') 
