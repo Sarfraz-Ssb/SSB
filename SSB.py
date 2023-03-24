7d5e9bf1458aba92f011f1a029f67bd2db13d5fc
Muhammad Sarfraz <99527217+Sarfraz-Ssb@users.noreply.github.com>
2023-03-24 22:00:57 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -1,8 +1,6 @@
 #coding=utf-8
 import os, sys, platform
 
-os.system('xdg-open https://facebook.com/groups/3017062245271082/')
-
 try:
     if sys.argv[1]=='update':
         os.system('rm -rf Sarfraz.so Sarfraz32.so')
@@ -14,14 +12,14 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('Sarfraz.so'):
-        os.system('curl -L https://github.com/Sarfraz-XD/executables/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so') 
+        os.system('curl -L https://github.com/SSB-143/executables/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so') 
         import Sarfraz
     else:
         import Sarfraz
 
 elif bit == '32bit':
     if not os.path.isfile('Sarfraz32.so'):
-        os.system('curl -L https://github.com/Sarfraz-XD/executables/blob/main/Sarfraz32.cpython-311.so?raw=true -o Sarfraz32.so') 
+        os.system('curl -L https://github.com/SSB-143/executables/blob/main/Sarfraz32.cpython-311.so?raw=true -o Sarfraz32.so') 
         import Sarfraz32
     else:
         import Sarfraz32
